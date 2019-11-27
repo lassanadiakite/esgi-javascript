790be18fd1ed6e2a7917c0c8324ca419ed4c7874
diakite lassana <lassanadiakite90@gmail.com>
2019-11-27 16:00:14 +0100

    snake_case bon

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -31,9 +31,23 @@ console.log(capitalize("test Test tst"));
 console.log(capitalize(""));
 console.log(capitalize(null));
 console.log(capitalize({}));
-function snake_case(str) { 
+function camelCase(str) { 
     if(typeof str !== "string" || !str) return '';
 
+    return str.toLowerCase().split(" ").map(function(item){
+        return ucfirst(item).split(/[^A-z]/g).join("");
+    }).join("");
+} 
+console.log(camelCase("test"));
+console.log(camelCase("Test"));
+console.log(camelCase(" test"));
+console.log(camelCase("test2 Test tst"));
+console.log(camelCase(""));
+console.log(camelCase(null));
+console.log(camelCase({}));
+function snake_case(str) { 
+    if(typeof str !== "string" || !str) return '';
+     str = str.trim();
     return str.toLowerCase().split(" ").join("_");
 } 
 console.log(snake_case("test"));
@@ -43,3 +57,4 @@ console.log(snake_case("test2 Test tst"));
 console.log(snake_case(""));
 console.log(snake_case(null));
 console.log(snake_case({}));
+
