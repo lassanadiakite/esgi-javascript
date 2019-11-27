72d696c3d08ae060585db16adab7cd09302e81b1
diakite lassana <lassanadiakite90@gmail.com>
2019-11-27 16:24:08 +0100

    leet bon

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -47,8 +47,8 @@ console.log(camelCase(null));
 console.log(camelCase({}));
 function snake_case(str) { 
     if(typeof str !== "string" || !str) return '';
-     str = str.trim();
-    return str.toLowerCase().split(" ").join("_");
+     
+    return str.toLowerCase().split(/[^a-zA-Z0-9]/).join("_");
 } 
 console.log(snake_case("test"));
 console.log(snake_case("Test"));
@@ -57,19 +57,14 @@ console.log(snake_case("test2 Test tst"));
 console.log(snake_case(""));
 console.log(snake_case(null));
 console.log(snake_case({}));
-var message = "Ceci est un message sans leet speak !";
+
  
-var messageLeet = "";
- 
-for (var i = 0; i < message.length; i++) {
-  if (message[i].toLowerCase() === "i") { messageLeet += "1"; }
-  else if (message[i].toLowerCase() === "e") { messageLeet += "3"; }
-  else if (message[i].toLowerCase() === "a") { messageLeet += "4"; }
-  else if (message[i].toLowerCase() === "t") { messageLeet += "7"; }
-  else if (message[i].toLowerCase() === "s") { messageLeet += "5"; }
-  else if (message[i].toLowerCase() === "o") { messageLeet += "0"; }
-  else { messageLeet += message[i].toLowerCase(); }
-}
- 
-console.log(messageLeet);
+function replaceChr(chr) {
+    var chrs = {'a':'4','e':'3','i':'1','t':'7','s':'5','o':'0'};
+    return chrs[chr.toLowerCase()] || chr;
+  }
+   
+  var message = "Ceci est un message sans leet speak !";
+   
+  console.log( message.replace(/[aeltso]/g, replaceChr) );
 
